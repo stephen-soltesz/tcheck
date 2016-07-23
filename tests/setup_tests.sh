9820b14b520d6ebbe424c109a799c940998357cb
Stephen Soltesz <stephen.soltesz@gmail.com>
2016-07-23 02:16:19 -0400

    Correct check for CentOS version.

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -4,7 +4,7 @@
 OS_VERSION=$1
 SPECFILE=$2
 
-if [ "$OS_VERSION" = "6" ]; then
+if [ "$OS_VERSION" = "6.6" ]; then
 
   sudo docker run --rm=true -v `pwd`:/repo:rw centos:centos${OS_VERSION} \
       /bin/bash -c "bash -xe /repo/tests/run_build.sh ${OS_VERSION} ${SPECFILE}"
