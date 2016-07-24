b67b087a4c30a16ee033562f5dbd106024e311b3
Stephen Soltesz <stephen.soltesz@gmail.com>
2016-07-24 19:02:13 -0400

    print environment.

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -3,12 +3,12 @@
 # CentOS version.
 OS_VERSION=$1
 SPECFILE=$2
+env
 
 # TRAVIS_REPO_SLUG=<owner>/<repo>
 if [ "$OS_VERSION" = "6.6" ]; then
 
   # sudo docker run --rm=true -v `pwd`:/repo:rw centos:centos${OS_VERSION} \
-  # 
   sudo docker run --rm=true \
       -v ${TRAVIS_BUILD_DIR}:/source:rw \
       toopher/centos-i386:centos6 /bin/bash -c \
