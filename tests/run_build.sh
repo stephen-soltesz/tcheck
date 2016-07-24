11a2af75aebfb51b67125c510cb5c0eb0df055b0
Stephen Soltesz <stephen.soltesz@gmail.com>
2016-07-24 18:30:03 -0400

    Disable setup-tests.sh and use "source" directory instead of repo.

--- tests/run_build.sh
+++ tests/run_build.sh
@@ -14,10 +14,9 @@ yum -y clean expire-cache
 
 # First, install all the needed packages.
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION%%.*}.noarch.rpm
-# Broken mirror?
-echo "exclude=mirror.beyondhosting.net" >> /etc/yum/pluginconf.d/fastestmirror.conf
 
-#yum -y update
+# TODO: enable update.
+# yum -y update
 yum -y clean all
 yum -y install yum-plugin-priorities
 yum -y install rpm-build gcc gcc-c++ cmake git tar gzip make autotools # boost-devel 
@@ -25,18 +24,15 @@ yum -y install rpm-build gcc gcc-c++ cmake git tar gzip make autotools # boost-d
 # Prepare the RPM environment
 mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 cat /etc/rpm/macros.dist
-cat >> /etc/rpm/macros.dist << EOF
-%dist .mlab.el6
-EOF
+# cat >> /etc/rpm/macros.dist << EOF
+# %dist .mlab.el6
+# EOF
 
-#cp /root/rpmbuild/RPMS/noarch/nodebase-0.1-0.noarch.rpm ${vdir}/root/
-#chroot ${vdir} rpm -ihv /root/nodebase-0.1-0.noarch.rpm 
+cp /source/${SPECFILE} /tmp/rpmbuild/SPECS
+package=$( rpm -q --specfile /source/${SPECFILE} --queryformat '%{Name}\n' )
+version=$( rpm -q --specfile /source/${SPECFILE} --queryformat '%{Version}\n' )
 
-cp /repo/${SPECFILE} /tmp/rpmbuild/SPECS
-package=$( rpm -q --specfile /repo/${SPECFILE} --queryformat '%{Name}\n' )
-version=$( rpm -q --specfile /repo/${SPECFILE} --queryformat '%{Version}\n' )
-# Repo directory must match the RPM spec file package name.
-pushd /repo
+pushd /source
   # CentOS 6 git version 1.7.1 does not support --format=tar.gz
   # Later versions of git do.
   git archive --format=tar --prefix=${package}-${version}/ HEAD | \
@@ -50,10 +46,10 @@ rpmbuild --define '_topdir /tmp/rpmbuild' -ba /tmp/rpmbuild/SPECS/${SPECFILE}
 yum localinstall -y /tmp/rpmbuild/RPMS/noarch/${package}*
 
 # Run unit tests on installed package.
-pushd /repo
+pushd /source
   echo "TODO: ADD UNIT TESTS HERE."
 popd
 
 # Copy RPM to location mounted outside of container.
-mkdir -p /repo/build
-cp /tmp/rpmbuild/RPMS/noarch/${package}* /repo/build
+mkdir -p /source/build
+cp /tmp/rpmbuild/RPMS/noarch/${package}* /source/build
--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -8,8 +8,11 @@ SPECFILE=$2
 if [ "$OS_VERSION" = "6.6" ]; then
 
   # sudo docker run --rm=true -v `pwd`:/repo:rw centos:centos${OS_VERSION} \
-  sudo docker run --rm=true -v `pwd`:/repo:rw toopher/centos-i386:centos6 \
-      /bin/bash -c "bash -xe /repo/tests/run_build.sh ${OS_VERSION} ${SPECFILE}"
+  # 
+  sudo docker run --rm=true \
+      -v ${TRAVIS_BUILD_DIR}:/source:rw \
+      toopher/centos-i386:centos6 /bin/bash -c \
+      "bash -xe /source/tests/run_build.sh ${OS_VERSION} ${SPECFILE}"
 
 else
 
